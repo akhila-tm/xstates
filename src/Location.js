02ab8e573c541902c7a97a3891e2b223496b3c03
akhila-tm <akhilatm1998@gmail.com>
2025-04-07 22:37:28 +0530

    initial commit

---
+++ src/Location.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+import './Location.css';
+
+function Location(){
+    let [countries,setCountries]=useState([])
+    let [states,setStates]=useState([])
+    let [cities,setCities]=useState([])
+
+    let [selectedCountry,setSelectedCountry]=useState("")
+    let [selectedState,setSelectedState]=useState("")
+    let [selectedCity,setSelectedCity]=useState("")
+
+
+    useEffect(()=>{
+        fetch("https://crio-location-selector.onrender.com/countries")
+        .then((response)=>response.json())
+        .then((data)=>{
+            setCountries(data)
+        })
+        .catch((error)=>{console.log(error)})
+    },[])
+
+    const handleCountryChange=(event)=>{
+        console.log(event.target.value)
+        setSelectedCountry(event.target.value)
+        getStates(event.target.value)
+    }
+    function getStates(payload){
+        fetch(`https://crio-location-selector.onrender.com/country=${payload}/states`)
+        .then((response) => response.json())
+        .then((data) => {
+          setStates(data)
+        })
+        .catch((error) => console.error(error));
+    }
+    const handleStateChange=(event)=>{
+        setSelectedState(event.target.value)
+        getCities(event.target.value)
+    }
+    const handleCityChange=(event)=>{
+        setSelectedCity(event.target.value)
+    }
+    function getCities(payload){
+        fetch(` https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${payload}/cities`)
+        .then((response) => response.json())
+        .then((data) => {
+          setCities(data)
+        })
+        .catch((error) => console.error(error));
+    }
+    return(<>
+    <h1>Select Location</h1>
+    <div className="dropdownFlex">
+    <select value={selectedCountry} onChange={handleCountryChange} >
+    <option value="" disabled>
+              Select Country
+            </option>
+        {countries.map((country,index)=>(
+        <option key={country} value={country}>{country}</option>
+        ))}
+    </select>
+    <select value={selectedState} onChange={handleStateChange} disabled={!selectedCountry}>
+    <option value="" disabled>
+              Select State
+            </option>
+        {states.map((state,index)=>(
+        <option key={state} value={state}>{state}</option>
+        ))}
+    </select>
+    <select value={selectedCity} onChange={handleCityChange} disabled={!selectedState}>
+    <option value="" disabled>
+              Select City
+            </option>
+        {cities.map((city,index)=>(
+        <option key={city} value={city}>{city}</option>
+        ))}
+    </select>
+    </div>
+    {selectedCountry && selectedCity && selectedState &&
+        <p>You selected <span className='selectedCity'>{selectedCity}</span>,<span className='selectedState'>{selectedState}</span> ,<span className='selectedState'>{selectedCountry}</span> </p>
+    }
+   
+    </>)
+}
+export default Location;
